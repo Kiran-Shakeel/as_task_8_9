50be4377b37abd670c88a9a881ae6f6ab1c8acc4
Kiran-Shakeel <kiranshakeel02@gmail.com>
2021-08-13 04:24:42 +0500

    done

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -11,45 +11,109 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final CollectionReference ref =
       FirebaseFirestore.instance.collection("augersoft");
-
   TextEditingController namecont = new TextEditingController();
   TextEditingController descont = new TextEditingController();
+
+  late String docId;
+  late String name, sub;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("CRUD Operations"),
-        centerTitle: true,
       ),
       body: StreamBuilder(
           stream: ref.snapshots(),
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            return ListView.separated(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(snapshot.data!.docs[index]['name']),
-                  subtitle: Text(snapshot.data!.docs[index]['subtitile']),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) => Divider(),
-            );
+            return !snapshot.hasData
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.separated(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot docRef = snapshot.data!.docs[index];
+
+                      return Column(children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.blue,
+                            child: Text(
+                                snapshot.data!.docs[index]['name']
+                                    .substring(0, 1)
+                                    .toUpperCase(),
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                          title: Text(
+                            snapshot.data!.docs[index]['name'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle:
+                              Text(snapshot.data!.docs[index]['subtitle']),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  name = snapshot.data!.docs[index]['name'];
+                                  sub = snapshot.data!.docs[index]['subtitle'];
+
+                                  docId = docRef.id;
+                                  _fetch(docId);
+                                },
+                                icon: Icon(
+                                  Icons.edit,
+                                  color: Colors.blue,
+                                )),
+                            IconButton(
+                              onPressed: () {
+                                docId = docRef.id;
+                                print(docId);
+                                _delete(docId);
+                              },
+                              icon: Icon(
+                                Icons.delete,
+                                color: Colors.blue,
+                              ),
+                            )
+                          ],
+                        ),
+                      ]);
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                        Divider(
+                      endIndent: 20.0,
+                      height: 20.0,
+                      indent: 20.0,
+                      color: Colors.blue,
+                    ),
+                  );
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _fetch();
+          docId = "";
+          _fetch(docId);
         },
         child: Icon(Icons.add),
       ),
     );
   }
 
-  _fetch() {
+  _fetch(String docId) {
     showDialog(
         context: context,
         builder: (BuildContext context) {
+          if (docId == "") {
+            namecont.text = "";
+            descont.text = "";
+          } else {
+            namecont.text = name;
+            descont.text = sub;
+          }
+
           return SimpleDialog(
             title: Text("Enter Information"),
             children: [
@@ -57,7 +121,9 @@ class _HomePageState extends State<HomePage> {
                 child: TextField(
                   controller: namecont,
                   decoration: InputDecoration(
-                      labelText: "UserName", hintText: "Enter Name"),
+                    labelText: "UserName",
+                    hintText: "Enter Name",
+                  ),
                 ),
               ),
               SimpleDialogOption(
@@ -70,10 +136,7 @@ class _HomePageState extends State<HomePage> {
               SimpleDialogOption(
                 child: ElevatedButton(
                     onPressed: () {
-                      ref.add({
-                        'name': namecont.text,
-                        'subtitile': descont.text,
-                      });
+                      docId == "" ? _addData() : _updateData(docId);
 
                       namecont.clear();
                       descont.clear();
@@ -84,5 +147,23 @@ class _HomePageState extends State<HomePage> {
             ],
           );
         });
+  }
+
+  Future<void> _delete(String docId) async {
+    await ref.doc(docId).delete();
+  }
+
+  Future<void> _addData() async {
+    await ref.add({
+      'name': namecont.text,
+      'subtitle': descont.text,
+    });
+  }
+
+  Future<void> _updateData(String docId) async {
+    await ref.doc(docId).set({
+      'name': namecont.text,
+      'subtitle': descont.text,
+    });
   }
 }
